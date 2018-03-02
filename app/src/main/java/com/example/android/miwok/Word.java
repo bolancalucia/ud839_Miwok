6aff60b088adf7ecfeaf42b48d1acf6c60d92c93
Lucia Bolanča <bolancalucia@gmail.com>
2018-03-02 18:26:49 +0100

    working on Miwok app

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,19 @@
+package com.example.android.miwok;
+
+public class Word {
+    private String mDefaultTranslation;
+    private String mMiwokTranslation;
+
+    public Word(String defaultTranslation, String miwokTranslation) {
+        mMiwokTranslation = miwokTranslation;
+        mDefaultTranslation = defaultTranslation;
+    }
+
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+}
